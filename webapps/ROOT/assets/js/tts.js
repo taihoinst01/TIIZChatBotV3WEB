54d55a5dca4374a1a5848dc44a2768700814deaa
장 상련 <srjang@taihoinst.com>
2019-03-19 13:33:55 +0900

    TTS 적용

--- webapps/ROOT/assets/js/tts.js
+++ webapps/ROOT/assets/js/tts.js
@@ -10,3 +10,17 @@ function lfn_speakTTS(text, speaker) {
     }
     bingClientTTS.synthesize(text, speaker);
 }
+
+//TTS 적용 장상련_20190319
+const synthesis = window.speechSynthesis;
+function speakTTS(word) {
+    const utter = new SpeechSynthesisUtterance(word.replace(/\/n*/gi, ''));
+    // the list of all available voices
+    const voices = synthesis.getVoices();
+
+
+    utter.voice = voices[13];
+    utter.rate = 1;
+    utter.pitch = 1;
+    synthesis.speak(utter);
+}
